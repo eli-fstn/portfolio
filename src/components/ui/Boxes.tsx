0565260869bb22ddb7805db9e59b021496e1dba7
skywie <festinelijah@gmail.com>
2026-08-31 00:15:54 +0800

    feat: AI chatbot that will act as me + responsiveness in all screen devices

--- src/components/ui/Boxes.tsx
+++ src/components/ui/Boxes.tsx
@@ -7,7 +7,7 @@ interface BoxProps {
 
 function Boxes({ text, typography, dimensions, designs}: BoxProps) {
   return (
-    <div className={`inline-flex items-center justify-center whitespace-normal break-words bg-[#0c0c0f] border border-[#2a2a30] font-mono text-[#8a8a92] transition duration-200 hover:scale-[1.02] cursor-pointer hover:text-[#f4f4f5] ${typography} ${dimensions} ${designs}`}>
+    <div className={`inline-flex items-center justify-center whitespace-normal wrap-break-word bg-[#0c0c0f] border border-[#2a2a30] font-mono text-[#8a8a92] transition duration-200 hover:scale-[1.02] cursor-pointer hover:text-[#f4f4f5] ${typography} ${dimensions} ${designs}`}>
       {text}
     </div>
   )
